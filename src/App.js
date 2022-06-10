18adb1fab2723cff9b306fe3c16bcfc08ce3a150
bilal882s <bilaliqbal78692110@gmail.com>
2022-06-09 21:01:27 -0700

    Breakpoint off

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       {/* <NavbarDark />*/}
       {/* <Buttons /> */}
       {/* <Cards />  */}
-      <Breakpoint />
+      {/* <Breakpoint /> */}
       {/* <Table /> */}
     </div>   
   );
