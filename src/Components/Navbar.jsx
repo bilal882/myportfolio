32ed30c478461af12d72b43c951af0a0ef00df34
bilal882s <bilaliqbal78692110@gmail.com>
2022-06-15 07:00:06 -0700

    Update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -23,36 +23,11 @@ export default function Navabr() {
             </a>
 
             <a class="navbar-item">
-              Documentation
+              About
             </a>
-
-            <div class="navbar-item has-dropdown is-hoverable">
-              <a class="navbar-link">
-                More
-              </a>
-
-              <div class="navbar-dropdown">
-                <a class="navbar-item">
-                  About
-                </a>
-                <a class="navbar-item">
-                  Jobs
-                </a>
-                <a class="navbar-item">
-                  Contact
-                </a>
-                <hr class="navbar-divider" />
-                <a class="navbar-item">
-                  Report an issue
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div class="navbar-end">
-            <div class="navbar-item">
-              {/* <p>Hi User</p> */}
-            </div>
+            <a class="navbar-item">
+              Contact
+            </a>
           </div>
         </div>
       </nav>
