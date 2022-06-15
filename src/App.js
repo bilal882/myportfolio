78cba977afff06cbb4aa63762076784040835a14
bilal882s <bilaliqbal78692110@gmail.com>
2022-06-15 05:00:40 -0700

    Hi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
+import Index from './Components/Form/Index';
 import Navabr from './Components/Navbar';
 import './Components/Style.css'
-// import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
-// import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma-rtl.min.css";
 function App() {
   return (
     <div>
-      <Navabr />
+      {/* <Navabr /> */}
+      <Index />
     </div>
   );
 }
---
+++ src/Components/Form/Form.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Form() {
+    return (
+        <div className='content'>
+            <h1 className='has-text-centered has-text-success'>Register Form</h1>
+            <input type="text" className='file-input content' placeholder='Name' />
+        </div>
+    )
+}
