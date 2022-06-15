e87d8a646c5deedbdaabe8f86d53e80f7be7dc88
bilal882s <bilaliqbal78692110@gmail.com>
2022-06-15 04:30:19 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
+import Navabr from './Components/Navbar';
+import './Components/Style.css'
+// import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
 // import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma-rtl.min.css";
 function App() {
   return (
     <div>
-      <button class="button is-primary">
-        Button
-      </button>
+      <Navabr />
     </div>
   );
 }
