7c5d32997d34aaa14828321c2243d20f99472725
bilal882s <bilaliqbal78692110@gmail.com>
2022-06-15 05:20:22 -0700

    Update

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -2,9 +2,31 @@ import React from 'react'
 
 export default function Form() {
     return (
-        <div className='content'>
-            <h1 className='has-text-centered has-text-success'>Register Form</h1>
-            <input type="text" className='file-input content' placeholder='Name' />
+        <div className='content m-5'>
+            <div class="field">
+                <label class="label">First Name</label>
+                <div class="control">
+                    <input class="input" type="text" placeholder="First Name" />
+                </div>
+            </div>
+
+            <div class="field">
+                <label class="label">Last Name</label>
+                <div class="control">
+                    <input class="input" type="text" placeholder="Last Name" required />
+                </div>
+            </div>
+            <div class="field">
+                <label class="label">Email</label>
+                <div class="control">
+                    <input class="input" type="email" placeholder="Email" required />
+                </div>
+            </div>
+            <div class="field is-grouped">
+                <div class="control">
+                    <input type='submit' class="button is-link" value="Submit"/>
+                </div>
+            </div>
         </div>
     )
 }
