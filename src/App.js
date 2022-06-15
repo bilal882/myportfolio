0d2c86edec8c0486fa1fb736f2c96a0673a75328
bilal882s <bilaliqbal78692110@gmail.com>
2022-06-15 03:32:40 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
+// import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma-rtl.min.css";
+function App() {
+  return (
+    <div>
+      <button class="button is-primary">
+        Button
+      </button>
+    </div>
+  );
+}
+
+export default App;
